20bb70f3b14e945a435dd00cf4ff9432f79f133a
Masa Watanabe <mcwata7@gmail.com>
2017-12-10 22:41:27 -0800

    Final Commit

--- tests/test_my_planning_graph.py
+++ tests/test_my_planning_graph.py
@@ -18,16 +18,18 @@ class TestPlanningGraphLevels(unittest.TestCase):
         self.pg = PlanningGraph(self.p, self.p.initial)
 
     def test_add_action_level(self):
-        # for level, nodeset in enumerate(self.pg.a_levels):
-        #     for node in nodeset:
-        #         print("Level {}: {}{})".format(level, node.action.name, node.action.args))
+        #print("\n")
+        #for level, nodeset in enumerate(self.pg.a_levels):
+        #    for node in nodeset:
+        #        print("Level {}: {}{})".format(level, node.action.name, node.action.args))
         self.assertEqual(len(self.pg.a_levels[0]), 3, len(self.pg.a_levels[0]))
         self.assertEqual(len(self.pg.a_levels[1]), 6, len(self.pg.a_levels[1]))
 
     def test_add_literal_level(self):
-        # for level, nodeset in enumerate(self.pg.s_levels):
-        #     for node in nodeset:
-        #         print("Level {}: {})".format(level, node.literal))
+        #print("\n")
+        #for level, nodeset in enumerate(self.pg.s_levels):
+        #    for node in nodeset:
+        #        print("Level {}: {}, {})".format(level, node.symbol, node.is_pos))
         self.assertEqual(len(self.pg.s_levels[0]), 2, len(self.pg.s_levels[0]))
         self.assertEqual(len(self.pg.s_levels[1]), 4, len(self.pg.s_levels[1]))
         self.assertEqual(len(self.pg.s_levels[2]), 4, len(self.pg.s_levels[2]))
